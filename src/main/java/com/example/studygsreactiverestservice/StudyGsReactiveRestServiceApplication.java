43a15f5c07151c828d6b21b4fe01d1d6fec831b3
Jaewook Choi <eari100@naver.com>
2022-12-19 15:41:43 +0900

    update: 빌드 시 GreetingClient.getMessage() 실행되도록 수정

--- src/main/java/com/example/studygsreactiverestservice/StudyGsReactiveRestServiceApplication.java
+++ src/main/java/com/example/studygsreactiverestservice/StudyGsReactiveRestServiceApplication.java
@@ -2,12 +2,15 @@ package com.example.studygsreactiverestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StudyGsReactiveRestServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudyGsReactiveRestServiceApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(StudyGsReactiveRestServiceApplication.class, args);
+        GreetingClient greetingClient = context.getBean(GreetingClient.class);
+        System.out.println(">> message = " + greetingClient.getMessage().block());
     }
 
 }
